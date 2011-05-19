e3984f0262cfbe1cd70f2ed591d7a239528eb73d
darklinger <darklinger@gmail.com>
2011-05-19 20:45:55 +0000

    Client joins Server in SVN. Client also happy.

---
+++ client/src/src/client/ui/utils/EventTypes.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src.client.ui.utils;
+
+/**
+ *
+ * @author Shachar
+ */
+        public enum EventTypes
+  {
+
+        GameStart(1),GameOver(2),GameWinner(3),PlayerResigned(4),PlayerLost(5),
+        PromptPlayerToRollDice (6),DiceRoll(7),Move(8),PassedStartSquare(9),LandedOnStartSquare(10),
+        GoToJail(11),PromptPlayerToBuyAsset(12),PromptPlayerToBuyHouse(13),AssetBoughtMessage(14),HouseBoughtMessage(15),
+        SurpriseCard(16),WarrantCard(17),GetOutOfJailCard(18),Payment(19),PlayerUsedJailCard(20);
+
+        private int code;
+
+        public  int getCode(){
+            return code;
+        }
+
+        private EventTypes(int code) {
+            this.code = code;
+        }
+    }
+
