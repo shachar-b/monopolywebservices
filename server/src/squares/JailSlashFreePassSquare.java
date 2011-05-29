d35fa14f564519bab5714bbc79c3eceae12bb411
blame.me@gmail.com <blame.me@gmail.com@88fc149a-d200-7c02-8ad1-b4670b16238c>
2011-05-29 08:01:07 +0000

    found that mother fucker- double dice prompt was not related to the card - it was left over code in the case of a double that fucked us

--- server/src/squares/JailSlashFreePassSquare.java
+++ server/src/squares/JailSlashFreePassSquare.java
@@ -64,6 +64,5 @@ public class JailSlashFreePassSquare extends Square {
         } else {
             player.setGoOnNextTurn(false);
         }
-        GameManager.currentGame.eventDispatch(player.getID(), "endTurn");
     }
 }
