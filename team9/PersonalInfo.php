b21b1f10125533238ebb363b54915b71e207883b
blame.me <blame.me@gmail.com>
2012-07-11 18:24:44 +0000

    backing up

--- team9/PersonalInfo.php
+++ team9/PersonalInfo.php
@@ -1,4 +1,8 @@
-<?php include_once 'DataBase.php'; ?>
+<?php
+include_once 'personalInfoPageGenerator.php';
+
+$genaretor = new personalInfoPageGenerator($_POST['queriedUserID'], $_POST['queryingUserID']);
+?>
 
 <!DOCTYPE html>
 <html>
@@ -9,58 +13,30 @@
         <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
         <TITLE>Personal Info Screen</TITLE>
         <script type="text/javascript">
-            function disableAllInputs(){
-                $(":input").attr("disabled", "disabled");
-            }
-            
-            function enableAllInputs(){
-                $(":input").removeAttr("disabled", "disabled");
-            }
-            
-            $(document).ready(function() {
-                disableAllInputs();
-                var queriedUserJSON = <?php getUserFromDatabase($_POST["queriedUserID"]) ?>;
-                var queryingUserJSON = <?php getUserFromDatabase($_POST["queryingUserID"]) ?>; //TODO : Take this from browser session.
-                var queriedUserID = queriedUserJSON["ID"];
-                var queryingUserID = queryingUserJSON["ID"];
-                $('#firstNameLbl').val(queriedUserJSON["FirstName"]);
-                $('#lastNameLbl').val(queriedUserJSON["LastName"]);
-                $('#phoneLbl').val(queriedUserJSON["Phone"]);
-                $('#emailLbl').val(queriedUserJSON["E-mail"]);
-                $('#userNameLbl').val(queriedUserJSON["UserName"]);
-                $('#passwordLbl').val(queriedUserJSON["Password"]);
-                $('#rankLbl').val(queriedUserJSON["Rank"]);
-                $('#permisLbl').val(queriedUserJSON["Permission"]);
-            });
+
+
+
+
+
+<?php
+echo $genaretor->getInitScripttAccordingToViewingPreviliges();
+echo $genaretor->getJSFunctionsAccordingToPremmisions();
+?>
+
+    $(document).ready(init);
         </script>
     </head>
     <body>
-        <form id="PersonalInfoForm">
-            <div class="header"><p>Personal Info Screen: </p></div>
-            <dl class="dl-horizontal">
-                <dt>Username</dt>
-                <dd><input id="userNameLbl" type="text"/></dd>
-                <dt>First Name</dt>
-                <dd><input id="firstNameLbl" type="text"/></dd>
-                <dt>Last Name</dt>
-                <dd><input id="lastNameLbl" type="text"/></dd>
-                <dt>Phone</dt>
-                <dd><input id="phoneLbl" type="text"/></dd>
-                <dt>eMail</dt>
-                <dd><input id="emailLbl" type="text"/></dd>
-                <dt>Password</dt>
-                <dd><input id="passwordLbl" type="text"/></dd>
-                <dt>Rank of user:</dt>
-                <dd><input id="rankLbl" type="text"/></dd>
-                <dt>Permission of employee:</dt>
-                <dd><input id="permisLbl" type="text"/></dd>
-            </dl>
-            <div class="btn-group">
-                <button class="btn" id="editBtn" disabled>Edit</button>
-                <button class="btn" id="saveBtn" disabled>Save</button>
-                <button class="btn" id="cancelBtn" disabled>Cancel</button>
+
+
+        <div id='PersonalInfoForm'>
+            <div class='header'><p>Personal Info Screen: </p></div>
+            <?php echo $genaretor->getUserDataListIfAllowed(); ?>
+            <div id="errorMSG">
             </div>
-        </form>
+            <div id="sucsessMSG">
+            </div>
+        </div>
     </body>
 </html>
 
@@ -73,15 +49,3 @@
             //Client accessing - can view own details (read only)
         }
 -->
-
-<?php
-
-function getUserFromDatabase($i_UserID) {
-    // @var Database
-    $dataBase = DataBase::getInstance();
-    $tableName = $dataBase->getTableForPersonID($i_UserID);
-    $className = $dataBase->getClassNameForTable($tableName);
-    $returnVal = $dataBase->getObjectForClass("ID = " . $i_UserID, $tableName, $className);
-    echo $returnVal->getJSON();
-}
-?>
